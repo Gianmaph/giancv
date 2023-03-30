fb622cecf1a1464aba3a5a356c1daa8340f158ad
Gianmarco <gianmarco.cricchio02@gmail.com>
2023-03-30 15:54:38 +0200

    Background element in Main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Content from "./components/content/Content";
+import Background from "./components/background/Background";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
