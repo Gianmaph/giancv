7aba690cb76d3095b1ef5e44b53c6489d9904764
Gianmarco <gianmarco.cricchio02@gmail.com>
2023-03-30 15:58:04 +0200

    main.jsx edited

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Content from "./components/content/Content";
 import Background from "./components/background/Background";
-import "./main.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div>
       <Background />
-      <div>
+      <div className="context">
         <Content />
       </div>
     </div>
