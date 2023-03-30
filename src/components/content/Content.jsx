302022c4c0dd72830b614c9ca7e7437769bc3d4b
Gianmarco <gianmarco.cricchio02@gmail.com>
2023-03-30 15:44:55 +0200

    Content element created

---
+++ src/components/content/Content.jsx
@@ -0,0 +1,11 @@
+import "./content.css";
+import MyPerson from "../myPerson/MyPerson";
+const Content = () => {
+  return (
+    <div className="content">
+      <MyPerson />
+    </div>
+  );
+};
+
+export default Content;
