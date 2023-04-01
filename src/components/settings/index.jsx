50a4c27d57205e3d7c4fd4b2d87312b4eba5d91a
Gianmarco <gianmarco.cricchio02@gmail.com>
2023-04-01 17:06:41 +0200

    Profile settings updates

--- src/components/settings/index.jsx
+++ src/components/settings/index.jsx
@@ -2,11 +2,18 @@ import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import { getDatabase, ref, set, onValue } from "firebase/database";
 import { db } from "../../../firebase/firebase.config";
+
 const Settings = () => {
   const [name, setName] = useState("");
   const [surname, setSurname] = useState("");
   const [age, setAge] = useState("");
-  const [roles, setRoles] = useState([]);
+  const [birth, setBirth] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  const [cap, setCap] = useState("");
+  const [address, setAddress] = useState("");
 
   // useEffect(() => {
   //   console.log(input);
@@ -17,7 +24,13 @@ const Settings = () => {
       name: name,
       surname: surname,
       age: age,
-      roles: roles,
+      birth: birth,
+      email: email,
+      number: number,
+      country: country,
+      city: city,
+      cap: cap,
+      address: address,
     };
     e.preventDefault();
     writeSettings(form);
@@ -25,49 +38,99 @@ const Settings = () => {
 
   function writeSettings(form) {
     const db = getDatabase();
-    set(ref(db, "settings"), {
-      name: form.name,
-      surname: form.surname,
-      age: form.age,
-      roles: form.roles,
-    });
+    set(ref(db, "settings/userInfo"), form);
   }
   useEffect(() => {
-    const query = ref(db, "settings");
+    const query = ref(db, "settings/userInfo");
     return onValue(query, (snapshot) => {
       const data = snapshot.val();
       setName(data.name);
       setSurname(data.surname);
       setAge(data.age);
-      setRoles(data.roles);
     });
   }, []);
 
   return (
     <div className={styles.main}>
       <div className={styles.settings}>
-        <h1 className={styles.title}>Impostazioni Profilo</h1>
+        <h1 className={styles.title}>Edit Profile</h1>
         <form onSubmit={handleForm}>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="text"
-            value={surname}
-            onChange={(e) => setSurname(e.target.value)}
-          />
-          <input
-            type="text"
-            value={age}
-            onChange={(e) => setAge(e.target.value)}
-          />
-          <input
-            type="text"
-            value={roles}
-            onChange={(e) => setRoles([e.target.value])}
-          />
+          <div className={styles.section}>
+            <div className={styles.subsection}>
+              <div className={styles.textbox}>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className={styles.textbox}>
+                <input
+                  type="text"
+                  value={surname}
+                  onChange={(e) => setSurname(e.target.value)}
+                />
+              </div>
+              <div className={styles.textbox}>
+                <input
+                  type="text"
+                  value={age}
+                  onChange={(e) => setAge(e.target.value)}
+                />
+              </div>
+              <div className={styles.textbox}>
+                <input
+                  type="text"
+                  value={birth}
+                  onChange={(e) => setBirth(e.target.value)}
+                />
+              </div>
+              <div className={styles.textbox}>
+                <input
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className={styles.subsection}>
+              <div className={styles.textbox}>
+                <input
+                  type="text"
+                  value={number}
+                  onChange={(e) => setNumber(e.target.value)}
+                />
+              </div>
+              <div className={styles.textbox}>
+                <input
+                  type="text"
+                  value={country}
+                  onChange={(e) => setCountry(e.target.value)}
+                />
+              </div>
+              <div className={styles.textbox}>
+                <input
+                  type="text"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                />
+              </div>
+              <div className={styles.textbox}>
+                <input
+                  type="text"
+                  value={cap}
+                  onChange={(e) => setCap(e.target.value)}
+                />
+              </div>
+              <div className={styles.textbox}>
+                <input
+                  type="text"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
           <button type="submit">Invia</button>
         </form>
       </div>
