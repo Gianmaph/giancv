5800a74f09522ddda2011c06269ed4d59ac21c74
Gianmarco <gianmarco.cricchio02@gmail.com>
2023-04-05 10:04:35 +0200

    Add work button

---
+++ src/components/settings/workinfo/addJob/index.jsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.scss";
+const AddJob = () => {
+  return (
+    <>
+      <button className={styles.addButton}>ADD WORK</button>
+    </>
+  );
+};
+
+export default AddJob;
--- src/components/settings/workinfo/index.jsx
+++ src/components/settings/workinfo/index.jsx
@@ -3,7 +3,7 @@ import { getDatabase, ref, set, onValue } from "firebase/database";
 import { db } from "../../../../firebase/firebase.config";
 import { useState, useEffect } from "react";
 import JobElement from "./jobElement";
-
+import AddJob from "./addJob";
 const Workinfo = ({ select }) => {
   //   const [activities, setActivities] = useState("");
   //   const [company, setCompany] = useState("");
@@ -14,6 +14,19 @@ const Workinfo = ({ select }) => {
   //   const [role, setRole] = useState("");
   //   const [start, setStart] = useState("");
   const [data, setData] = useState([]);
+  const [showWork, setShowWork] = useState(true);
+  const [showJob, setShowJob] = useState(false);
+
+  const addJob = () => {
+    if (showWork === true) {
+      setShowWork(false);
+      setShowJob(true);
+    } else if (showWork === false) {
+      setShowWork(true);
+      setShowJob(false);
+    }
+  };
+
   useEffect(() => {
     const query = ref(db, "settings/workInfo");
     return onValue(query, (snapshot) => {
@@ -32,10 +45,17 @@ const Workinfo = ({ select }) => {
   }, []);
 
   return (
-    <div className={select ? styles.workinfo : styles.noworkinfo}>
-      <JobElement data={data} />
-      <button className={styles.addButton}>ADD WORK</button>
-    </div>
+    <>
+      <div className={select && showWork ? styles.workinfo : styles.noworkinfo}>
+        <JobElement data={data} />
+        <button onClick={addJob} className={styles.addButton}>
+          ADD WORK
+        </button>
+      </div>
+      <div className={showJob ? styles.addJob : styles.noaddJob}>
+        <AddJob />
+      </div>
+    </>
   );
 };
 
